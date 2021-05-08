c2f955132cf78f45ad5f03882b3bb9733d292bdd
vk3dan <dangaz@gmail.com>
2021-05-08 20:23:10 +1000

    gotta be close now lol

--- yeetmytweet.py
+++ yeetmytweet.py
@@ -13,20 +13,16 @@ Copyright 2021 Dan Phillips
 api = twitter.Api(consumer_key=config.consumer_key,
                   consumer_secret=config.consumer_secret,
                   access_token_key=config.access_token_key,
-                  access_token_secret=config.access_token_secret,
-                  sleep_on_rate_limit=True)
+                  access_token_secret=config.access_token_secret)
 
 current_date = datetime.now()
 daysago_date = current_date - timedelta(days=config.daysagotoyeet)
-range_start = datetime.strptime('Mar 20 00:00:00 +0000 2006','%b %d %H:%M:%S %z %Y')
-range_end = datetime.strftime(daysago_date,'%b %d %H:%M:%S %z %Y')
-username=""
+range_start = datetime.strptime('Mon Mar 20 00:00:00 +0000 2006','%a %b %d %H:%M:%S %z %Y')
+range_end = datetime.strftime(daysago_date,'%a %b %d %H:%M:%S +0000 %Y')
 helptext = """
-    Usage: yeetmytweet.py <-u someusername> <-i/-a>
-           yeetmytweet.py <--username=someusername> <--interactive/--auto> 
+    Usage: yeetmytweet.py <-i/-a>
+           yeetmytweet.py <--interactive/--auto> 
     Options:
-        Note that order is important, username option MUST be first.
-        -u (--username)    : Username         : (Followed by the username you are operating on)
         -i (--interactive) : Interactive mode : Asks for confirmation before deleting tweets
         -a (--auto)        : Automatic mode   : For use in a cronjob to automatically delete tweets
         -h (--help)        : Help             : Displays this help message
@@ -41,7 +37,7 @@ def main(argv):
         print(helptext)
         sys.exit(1)
     try:
-        opts, args = getopt.getopt(argv,"hu:ia",['help','username=','interactive','auto'])
+        opts, args = getopt.getopt(argv,"hia",['help','interactive','auto'])
     except getopt.GetoptError:
         print("ERROR: Unknown argument(s)")
         print(helptext)
@@ -50,22 +46,24 @@ def main(argv):
         if opt in ("-h", "--help"):
             print(helptext)
             sys.exit()
-        elif opt in ("-u", "--username"):
-            if arg=="":
-                print("no username specified. Exiting")
-                sys.exit(5)
-            username = arg
-            print(f"Username: {username}")
+#        elif opt in ("-u", "--username"):
+#            if arg=="":
+#                print("no username specified. Exiting")
+#                sys.exit(5)
+#            username = arg
+#            print(f"Username: {username}")
         elif opt in ("-i", "--interactive"):
-            print(f"This action will delete ALL tweets on account {username} created before : {datetime.strftime(daysago_date,'%b %d %Y')} (30 days ago).\n")
+            print(f"This action will delete ALL tweets on your account created before : {datetime.strftime(daysago_date,'%b %d %Y')} ({config.daysagotoyeet} days ago).\n")
             print("THIS ACTION NOT BE UNDONE!\n")
             confirmation=input("Type YES to continue (not case sensitive): ")
             if confirmation.upper() != "YES":
                 print("user failed to confirm destructive action. Exiting")
                 sys.exit(4)
+            else:
+                print("user confirmed destructive action. Starting delete.")
         elif opt in ("-a", "--auto"):
-            print(f"Automatic mode. This will delete ALL tweets on account {username} created before : {datetime.strftime(daysago_date,'%b %d %Y')} (30 days ago).\n")
-            cancelsec=10
+            print(f"Automatic mode. This will delete ALL tweets on your account created before : {datetime.strftime(daysago_date,'%b %d %Y')} ({config.daysagotoyeet} days ago).\n")
+            cancelsec=15
             try:
                 while cancelsec:
                     print(f"You have {cancelsec} to press Ctrl-C to cancel the yeet ", end='\r')
@@ -75,6 +73,19 @@ def main(argv):
                 print("\nOperation Cancelled by user before beginning.")
                 sys.exit()
             print("Starting automatic delete.")
- 
+    yeeted=0
+    while True:
+        print(f"Yeeted {yeeted} tweets \r")
+        ids=[]
+        tweets = api.GetUserTimeline(api.VerifyCredentials().id, count=200, include_rts=True)
+        for t in tweets:
+            if t.created_at <= range_end:
+                print(t.id)
+                api.DestroyStatus(t.id)
+                yeeted += 1
+            if yeeted==0:
+                print (f"All done, Exiting. Thanks for using yeetmytweet to yeet {yeeted} tweets")
+                sys.exit()
+
 if __name__ == "__main__":
-   main(sys.argv[1:])
+    main(sys.argv[1:])
